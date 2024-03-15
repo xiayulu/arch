9cb7ad7fba90eb7e028269bf6488127ab728f815
xiayulu <harolddfinchh@gmail.com>
2024-03-15 20:16:42 +0800

    fix

--- bin/install.sh
+++ bin/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 source shared/install.sh
-# source archlinuxcn/install.sh
-# source aur/install.sh
-# source clash/install.sh
-# source hyprland/install.sh
-# source im/install.sh
-# source waybar/install.sh
+source archlinuxcn/install.sh
+source aur/install.sh
+source clash/install.sh
+source hyprland/install.sh
+source im/install.sh
+source waybar/install.sh
 source mako/install.sh
 source tofi/install.sh
 source cliphist/install.sh
